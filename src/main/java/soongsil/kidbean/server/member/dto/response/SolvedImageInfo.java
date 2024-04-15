6c37c6b8cdb41358881169a7de8a9f7e6733b368
gikhoon <ghikhoon@naver.com>
2024-04-15 18:59:30 +0900

    feat: 이미지 퀴즈에 리스트에 정답, 카테고리도 들어가게 설정

--- src/main/java/soongsil/kidbean/server/member/dto/response/SolvedImageInfo.java
+++ src/main/java/soongsil/kidbean/server/member/dto/response/SolvedImageInfo.java
@@ -3,15 +3,20 @@ package soongsil.kidbean.server.member.dto.response;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import soongsil.kidbean.server.quiz.domain.QuizSolved;
+import soongsil.kidbean.server.quiz.domain.type.QuizCategory;
 
 @Builder
 public record SolvedImageInfo(
         Long solvedId,
+        QuizCategory quizCategory,
+        String answer,
         LocalDateTime solvedTime
 ) {
     public static SolvedImageInfo from(QuizSolved quizSolved) {
         return new SolvedImageInfo(
                 quizSolved.getSolvedId(),
+                quizSolved.getImageQuiz().getQuizCategory(),
+                quizSolved.getImageQuiz().getAnswer(),
                 quizSolved.getSolvedTime()
         );
     }
