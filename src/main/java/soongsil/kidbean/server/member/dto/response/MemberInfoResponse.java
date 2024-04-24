2b6fa34fed6ccb09a9ef65cbcbee6d277e6900cf
gikhoon <ghikhoon@naver.com>
2024-04-24 16:35:58 +0900

    feat: memberInfo 출력 API 개발

--- src/main/java/soongsil/kidbean/server/member/dto/response/MemberInfoResponse.java
+++ src/main/java/soongsil/kidbean/server/member/dto/response/MemberInfoResponse.java
@@ -3,12 +3,14 @@ package soongsil.kidbean.server.member.dto.response;
 import java.time.LocalDate;
 import java.time.Period;
 import soongsil.kidbean.server.member.domain.Member;
+import soongsil.kidbean.server.member.domain.type.Gender;
 
 public record MemberInfoResponse(
         Long memberId,
         LocalDate memberBirth,
         String memberName,
-        Integer memberAge
+        Integer memberAge,
+        Gender gender
 ) {
     public static MemberInfoResponse from(Member member) {
         int age = Period.between(member.getBirthDate(), LocalDate.now()).getYears();
@@ -17,7 +19,8 @@ public record MemberInfoResponse(
                 member.getMemberId(),
                 member.getBirthDate(),
                 member.getName(),
-                age
+                age,
+                member.getGender()
         );
     }
 }
