a8b07cb9e222dbbccc8cc48a4d1c2282480eff0e
LDY <leedy3838@naver.com>
2024-04-28 15:40:14 +0900

    refactor: 회원 가입을 하지 않으면 유저 등록 X로 수정

--- src/main/java/soongsil/kidbean/server/auth/application/CustomUserDetailsService.java
+++ src/main/java/soongsil/kidbean/server/auth/application/CustomUserDetailsService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import soongsil.kidbean.server.auth.dto.CustomUserDetails;
 import soongsil.kidbean.server.member.domain.Member;
 import soongsil.kidbean.server.member.exception.MemberNotFoundException;
