de1728698351233ce908a4d32dac9bf73fa326d2
gikhoon <ghikhoon@naver.com>
2024-04-29 22:26:08 +0900

    refactor: jwt 토큰 추가에 따른 MemberController, MypageController 변경

--- src/main/java/soongsil/kidbean/server/member/presentation/MemberController.java
+++ src/main/java/soongsil/kidbean/server/member/presentation/MemberController.java
@@ -2,10 +2,12 @@ package soongsil.kidbean.server.member.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import soongsil.kidbean.server.auth.dto.AuthUser;
 import soongsil.kidbean.server.global.dto.ResponseTemplate;
 import soongsil.kidbean.server.member.application.MemberService;
 
@@ -16,9 +18,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/info/{memberId}")
+    @GetMapping("/info")
     public ResponseEntity<ResponseTemplate<Object>> findMemberInfo(
-            @PathVariable(name = "memberId") Long memberId) {
-        return ResponseEntity.ok(ResponseTemplate.from(memberService.findMemberInfo(memberId)));
+            @AuthenticationPrincipal AuthUser user) {
+        return ResponseEntity.ok(ResponseTemplate.from(memberService.findMemberInfo(user.memberId())));
     }
 }
--- src/main/java/soongsil/kidbean/server/member/presentation/MypageController.java
+++ src/main/java/soongsil/kidbean/server/member/presentation/MypageController.java
@@ -2,10 +2,12 @@ package soongsil.kidbean.server.member.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import soongsil.kidbean.server.auth.dto.AuthUser;
 import soongsil.kidbean.server.global.dto.ResponseTemplate;
 import soongsil.kidbean.server.member.application.MypageService;
 
@@ -16,10 +18,10 @@ public class MypageController {
 
     private final MypageService mypageService;
 
-    @GetMapping("/image/list/{memberId}")
+    @GetMapping("/image/list")
     public ResponseEntity<ResponseTemplate<Object>> findSolvedImageList(
-            @PathVariable(name = "memberId") Long memberId) {
-        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findSolvedImage(memberId)));
+            @AuthenticationPrincipal AuthUser user) {
+        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findSolvedImage(user.memberId())));
     }
 
     @GetMapping("/image/{solvedId}")
@@ -28,10 +30,10 @@ public class MypageController {
         return ResponseEntity.ok(ResponseTemplate.from(mypageService.solvedImageDetail(solvedId)));
     }
 
-    @GetMapping("/word/list/{memberId}")
+    @GetMapping("/word/list")
     public ResponseEntity<ResponseTemplate<Object>> findSolvedWordList(
-            @PathVariable(name = "memberId") Long memberId) {
-        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findSolvedWord(memberId)));
+            @AuthenticationPrincipal AuthUser user) {
+        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findSolvedWord(user.memberId())));
     }
 
     @GetMapping("/word/{solvedId}")
@@ -40,11 +42,10 @@ public class MypageController {
         return ResponseEntity.ok(ResponseTemplate.from(mypageService.solvedWordDetail(solvedId)));
     }
 
-    @GetMapping("/voice/list/{memberId}")
+    @GetMapping("/voice/list")
     public ResponseEntity<ResponseTemplate<Object>> findSolvedVoiceList(
-            @PathVariable(name = "memberId") Long memberId
-    ) {
-        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findSolvedAnswerQuiz(memberId)));
+            @AuthenticationPrincipal AuthUser user) {
+        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findSolvedAnswerQuiz(user.memberId())));
     }
 
     @GetMapping("/voice/{solvedId}")
@@ -53,9 +54,9 @@ public class MypageController {
         return ResponseEntity.ok(ResponseTemplate.from(mypageService.solvedAnswerQuizDetail(solvedId)));
     }
 
-    @GetMapping("/image/result/{memberId}")
+    @GetMapping("/image/result")
     public ResponseEntity<ResponseTemplate<Object>> findImageQuizScoreInfo(
-            @PathVariable(name = "memberId") Long memberId) {
-        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findImageQuizScore(memberId)));
+            @AuthenticationPrincipal AuthUser user) {
+        return ResponseEntity.ok(ResponseTemplate.from(mypageService.findImageQuizScore(user.memberId())));
     }
 }
