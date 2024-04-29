f4238b8456be9257926ce18f1526109d6164fd7b
Amepistheo <kellylhji@gmail.com>
2024-04-29 22:17:21 +0900

    style: 아이 점수에 따른 url return에 대한 주석 추가 및 코드 수정 #43

--- src/main/java/soongsil/kidbean/server/member/application/type/HomeImageUrl.java
+++ src/main/java/soongsil/kidbean/server/member/application/type/HomeImageUrl.java
@@ -5,7 +5,8 @@ public enum HomeImageUrl {
     LEVEL2("https://kidbean.s3.ap-northeast-2.amazonaws.com/home/tree2.png", 200L),
     LEVEL3("https://kidbean.s3.ap-northeast-2.amazonaws.com/home/tree3.png", 300L),
     LEVEL4("https://kidbean.s3.ap-northeast-2.amazonaws.com/home/tree4.png", 400L),
-    LEVEL5("https://kidbean.s3.ap-northeast-2.amazonaws.com/home/tree5.png", Long.MAX_VALUE);
+    LEVEL5("https://kidbean.s3.ap-northeast-2.amazonaws.com/home/tree5.png", Long.MAX_VALUE),
+    ;
 
     private final String url;
     private final Long maxScore;
@@ -15,6 +16,10 @@ public enum HomeImageUrl {
         this.maxScore = maxScore;
     }
 
+    /**
+     * 아이의 점수를 받아와 maxScore과 비교를 통해 범위에 해당하는 이미지 url return
+     * default는 LEVEL1의 url return (null 포함)
+     */
     public static String getUrlByScore(Long score) {
         for (HomeImageUrl imageUrl : HomeImageUrl.values()) {
             if (score <= imageUrl.maxScore) {
