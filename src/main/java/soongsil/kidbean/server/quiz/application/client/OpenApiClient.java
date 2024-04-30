3b2aea41dcb7b9da8007f9d1a0be0250a5378a5f
LDY <leedy3838@naver.com>
2024-04-30 13:04:38 +0900

    test: todo 생성 -> 추후 테스트 코드 작성

--- src/main/java/soongsil/kidbean/server/quiz/application/client/OpenApiClient.java
+++ src/main/java/soongsil/kidbean/server/quiz/application/client/OpenApiClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import soongsil.kidbean.server.quiz.application.vo.ApiResponseVO;
 
+//TODO 이 부분은 나중에 refactoring 할 때 추가로 WireMockServer 로 테스트 하기
 @FeignClient(name = "openApiClient", url = "${openApi.url}", configuration = OpenApiFeignClientConfig.class)
 public interface OpenApiClient {
 
