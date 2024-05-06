9c0e9ce912752347c5022cbbab81bd265564b276
Amepistheo <kellylhji@gmail.com>
2024-05-06 16:56:27 +0900

    feat: 이미지 문제 등록하기 Test 코드 완성! #56

--- src/test/java/soongsil/kidbean/server/quiz/application/ImageQuizServiceTest.java
+++ src/test/java/soongsil/kidbean/server/quiz/application/ImageQuizServiceTest.java
@@ -2,9 +2,10 @@ package soongsil.kidbean.server.quiz.application;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static soongsil.kidbean.server.member.fixture.MemberFixture.MEMBER1;
 import static soongsil.kidbean.server.quiz.fixture.ImageQuizFixture.IMAGE_QUIZ_ANIMAL1;
 
@@ -17,11 +18,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import soongsil.kidbean.server.global.application.S3Uploader;
 import soongsil.kidbean.server.member.domain.Member;
 import soongsil.kidbean.server.member.repository.MemberRepository;
 import soongsil.kidbean.server.quiz.domain.ImageQuiz;
+import soongsil.kidbean.server.quiz.domain.type.QuizCategory;
+import soongsil.kidbean.server.quiz.dto.request.ImageQuizUploadRequest;
 import soongsil.kidbean.server.quiz.dto.response.ImageQuizMemberDetailResponse;
 import soongsil.kidbean.server.quiz.dto.response.ImageQuizMemberResponse;
 import soongsil.kidbean.server.quiz.dto.response.ImageQuizSolveListResponse;
@@ -36,8 +43,15 @@ class ImageQuizServiceTest {
     @Mock
     private MemberRepository memberRepository;
 
+    @Mock
+    private S3Uploader s3Uploader;
+
     @InjectMocks
     private ImageQuizService imageQuizService;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private final String COMMON_URL = "example.com";
+    private final String folderName = "quiz/NONE";
 
     @Test
     @DisplayName("ImageQuizResponse 생성 테스트")
@@ -83,5 +97,37 @@ class ImageQuizServiceTest {
 
         // then
         assertThat(response.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("ImageQuiz 등록하기")
+    void uploadImageQuiz() {
+        // given
+        ImageQuizUploadRequest request = ImageQuizUploadRequest.builder()
+                .title("테스트 제목")
+                .answer("테스트 정답")
+                .quizCategory(QuizCategory.NONE)
+                .build();
+
+        // MultipartFile 생성
+        MockMultipartFile file = new MockMultipartFile(
+                "file", // form의 input file name
+                "filename.jpg", // 파일명
+                "text/plain", // 컨텐츠 타입
+                "This is the file content".getBytes() // 파일 컨텐츠
+        );
+
+        String s3Url = "https://kidbean.s3.ap-northeast-2.amazonaws.com/quiz/NONE/filename.jpg";
+
+        given(memberRepository.findById(any(Long.class))).willReturn(Optional.of(MEMBER1));
+        given(s3Uploader.upload(any(MultipartFile.class), anyString())).willReturn(s3Url);
+
+        // when
+        imageQuizService.uploadImageQuiz(request, 1L, file);
+
+        // then
+        verify(memberRepository, times(1)).findById(MEMBER1.getMemberId());
+        verify(s3Uploader, times(1)).upload(any(MultipartFile.class), anyString());
+        verify(imageQuizRepository, times(1)).save(any(ImageQuiz.class));
     }
 }
