6cb308120b5aaa377a772af1f942332aaf973ee2
Amepistheo <kellylhji@gmail.com>
2024-05-11 22:56:21 +0900

    refactor: MemberService 코드 리팩토링 #56

--- src/main/java/soongsil/kidbean/server/member/application/MemberService.java
+++ src/main/java/soongsil/kidbean/server/member/application/MemberService.java
@@ -1,7 +1,5 @@
 package soongsil.kidbean.server.member.application;
 
-import static soongsil.kidbean.server.member.exception.errorcode.MemberErrorCode.MEMBER_NOT_FOUND;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,6 +10,8 @@ import soongsil.kidbean.server.member.dto.response.MemberInfoResponse;
 import soongsil.kidbean.server.member.exception.MemberNotFoundException;
 import soongsil.kidbean.server.member.repository.MemberRepository;
 
+import static soongsil.kidbean.server.member.exception.errorcode.MemberErrorCode.MEMBER_NOT_FOUND;
+
 @Slf4j
 @Service
 @Transactional(readOnly = true)
@@ -19,20 +19,21 @@ import soongsil.kidbean.server.member.repository.MemberRepository;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
     public MemberInfoResponse findMemberInfo(Long memberId) {
         Member member = findMemberById(memberId);
 
         return MemberInfoResponse.from(member);
     }
 
-    private Member findMemberById(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(()-> new MemberNotFoundException(MEMBER_NOT_FOUND));
-    }
-
     @Transactional
     public void uploadMemberInfo(MemberInfoRequest request, Long memberId) {
         Member member = findMemberById(memberId);
         member.uploadMember(request.name(), request.gender(), request.birthDate());
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(()-> new MemberNotFoundException(MEMBER_NOT_FOUND));
     }
 }
