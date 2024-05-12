c8c2de96c9f8a90a181ba078f14782b4820aad51
Amepistheo <kellylhji@gmail.com>
2024-05-12 13:19:38 +0900

    refactor: merge 오류 처리중

--- src/main/java/soongsil/kidbean/server/member/dto/response/SolvedAnswerDetailResponse.java
+++ src/main/java/soongsil/kidbean/server/member/dto/response/SolvedAnswerDetailResponse.java
@@ -1,10 +1,7 @@
 package soongsil.kidbean.server.member.dto.response;
 
-import java.util.List;
-import soongsil.kidbean.server.quiz.application.vo.ApiResponseVO.ReturnObject.Sentence.MorphemeVO;
 import soongsil.kidbean.server.quiz.application.vo.MorphemeCheckListResponse;
 import soongsil.kidbean.server.quiz.domain.AnswerQuizSolved;
-import soongsil.kidbean.server.quiz.domain.Morpheme;
 
 public record SolvedAnswerDetailResponse(
         Long solvedId,
--- src/main/java/soongsil/kidbean/server/program/presentation/ProgramController.java
+++ src/main/java/soongsil/kidbean/server/program/presentation/ProgramController.java
@@ -13,13 +13,11 @@ import org.springframework.web.multipart.MultipartFile;
 import soongsil.kidbean.server.auth.dto.AuthUser;
 import soongsil.kidbean.server.global.dto.ResponseTemplate;
 import soongsil.kidbean.server.program.application.ProgramService;
-import soongsil.kidbean.server.program.domain.Program;
 import soongsil.kidbean.server.program.domain.type.ProgramCategory;
 import soongsil.kidbean.server.program.dto.request.EnrollProgramRequest;
-import soongsil.kidbean.server.program.dto.response.ProgramListResponse;
-import soongsil.kidbean.server.program.dto.response.ProgramDetailResponse;
 import soongsil.kidbean.server.program.dto.request.UpdateProgramRequest;
-import soongsil.kidbean.server.program.repository.ProgramRepository;
+import soongsil.kidbean.server.program.dto.response.ProgramDetailResponse;
+import soongsil.kidbean.server.program.dto.response.ProgramListResponse;
 
 import static soongsil.kidbean.server.global.dto.ResponseTemplate.EMPTY_RESPONSE;
 
@@ -30,32 +28,32 @@ public class ProgramController {
 
     private static final int PAGE_SIZE = 4;
     private final ProgramService programService;
-    private final ProgramRepository programRepository;
 
     //목록조회 -> 페이징 진행
     @GetMapping("/programs")
-    public ResponseEntity<ProgramListResponse> getProgramListInfo(
+    public ResponseEntity<ResponseTemplate<Object>> getProgramListInfo(
             @RequestParam ProgramCategory programcategory,
             @AuthenticationPrincipal AuthUser user,
             @RequestParam(defaultValue = "0") int page) {
+
         Pageable pageRequest = PageRequest.of(page, PAGE_SIZE);
+        ProgramListResponse response = programService.getProgramListInfo(user.memberId(), programcategory, pageRequest);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(programService.getProgramListInfo(user.memberId() ,programcategory, pageRequest));
+                .body(ResponseTemplate.from(response));
     }
 
     //상세조회
     @GetMapping("/programs/{programId}")
-    public ResponseEntity<ProgramDetailResponse> getProgramInfo(
+    public ResponseEntity<ResponseTemplate<Object>> getProgramInfo(
             @PathVariable Long programId) {
 
-        Program program = programRepository.findById(programId)
-                .orElseThrow(RuntimeException::new);
+        ProgramDetailResponse response = programService.getProgramInfo(programId);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(programService.getProgramInfo(programId));
+                .body(ResponseTemplate.from(response));
     }
 
 
@@ -66,6 +64,7 @@ public class ProgramController {
             @PathVariable Long programId) {
 
         programService.deleteProgram(programId);
+
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(EMPTY_RESPONSE);
@@ -94,10 +93,9 @@ public class ProgramController {
             @AuthenticationPrincipal AuthUser user,
             @Valid @RequestPart EnrollProgramRequest enrollProgramRequest,
             @RequestPart MultipartFile programS3Url,
-            @RequestPart MultipartFile teacherS3Url
-    ){
+            @RequestPart MultipartFile teacherS3Url) {
 
-        programService.createProgram(enrollProgramRequest,programS3Url,teacherS3Url);
+        programService.createProgram(enrollProgramRequest, programS3Url, teacherS3Url);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
