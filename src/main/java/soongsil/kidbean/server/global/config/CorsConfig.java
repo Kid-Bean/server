222394e1cd84af788f781a775d986eebf9b76a12
LDY <leedy3838@naver.com>
2024-05-15 13:18:37 +0900

    feat: cors url yml로 이동

--- src/main/java/soongsil/kidbean/server/global/config/CorsConfig.java
+++ src/main/java/soongsil/kidbean/server/global/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package soongsil.kidbean.server.global.config;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,12 +12,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed.origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://kidbean.store"));
+        config.setAllowedOrigins(List.of(allowedOrigins));
         config.setAllowedMethods(List.of(
                 HttpMethod.POST.name(),
                 HttpMethod.GET.name(),
