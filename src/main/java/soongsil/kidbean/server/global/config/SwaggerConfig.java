c0d4f241a422b932d7ee8f2012f74a949212fc7b
LDY <leedy3838@naver.com>
2024-05-15 12:45:57 +0900

    fix: swagger cors 관련 수정

--- src/main/java/soongsil/kidbean/server/global/config/SwaggerConfig.java
+++ src/main/java/soongsil/kidbean/server/global/config/SwaggerConfig.java
@@ -2,26 +2,23 @@ package soongsil.kidbean.server.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import java.util.Collections;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(title = "Kid@Bean API 명세서",
                 description = "Kid@Bean API 명세서",
-                version = "v1"))
+                version = "v1"),
+        servers = @Server(url = "/", description = "Default Server URL") // Server 정보 추가
+)
 @Configuration
 public class SwaggerConfig {
-
-    @Value("${server.url}")
-    private String serverUrl;
 
     //JWT 사용시 swagger 사용 가능하게
     @Bean
@@ -31,12 +28,8 @@ public class SwaggerConfig {
                 .in(SecurityScheme.In.HEADER).name("Authorization");
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
-        Server server = new Server();
-        server.setUrl(serverUrl); // https://에 접근 가능하게 설정
-
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(Collections.singletonList(securityRequirement))
-                .servers(List.of(server));
+                .security(Collections.singletonList(securityRequirement));
     }
 }
