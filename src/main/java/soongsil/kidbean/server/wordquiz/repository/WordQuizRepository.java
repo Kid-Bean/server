e4e2ef5f6748e3e0892cc6f6db5c1c45b4733bd9
Amepistheo <kellylhji@gmail.com>
2024-07-30 16:42:16 +0900

    refactor: N+1 문제 및 OOM 1차 해결 #105

--- src/main/java/soongsil/kidbean/server/wordquiz/repository/WordQuizRepository.java
+++ src/main/java/soongsil/kidbean/server/wordquiz/repository/WordQuizRepository.java
@@ -1,6 +1,6 @@
 package soongsil.kidbean.server.wordquiz.repository;
 
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,8 +16,9 @@ public interface WordQuizRepository extends JpaRepository<WordQuiz, Long> {
     @Query("SELECT count(*) FROM WordQuiz wq WHERE wq.member = :member OR wq.member.role = 'ADMIN'")
     Integer countByMemberOrAdmin(Member member);
 
-    @Query("SELECT wq FROM WordQuiz wq WHERE wq.member = :member OR wq.member.role = 'ADMIN'")
-    List<WordQuiz> findSingleResultByMember(Member member, Pageable pageable);
+    @EntityGraph(attributePaths = {"words"})
+    @Query("SELECT wq FROM WordQuiz wq WHERE wq.quizId = :quizId AND wq.member = :member OR wq.member.role = 'ADMIN'"  )
+    List<WordQuiz> findSingleResultByMember(Member member, Long quizId);
 
     Optional<WordQuiz> findByQuizIdAndMember_MemberId(Long quizId, Long memberId);
 
