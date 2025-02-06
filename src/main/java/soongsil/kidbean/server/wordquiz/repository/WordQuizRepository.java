21be78439aeb79962148a9fd84f47cbf7d25c297
Amepistheo <kellylhji@gmail.com>
2025-02-06 22:16:48 +0900

    refactor: EntityGraph -> Join Fetch로 변경 #105

--- src/main/java/soongsil/kidbean/server/wordquiz/repository/WordQuizRepository.java
+++ src/main/java/soongsil/kidbean/server/wordquiz/repository/WordQuizRepository.java
@@ -1,6 +1,5 @@
 package soongsil.kidbean.server.wordquiz.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,8 +15,7 @@ public interface WordQuizRepository extends JpaRepository<WordQuiz, Long> {
     @Query("SELECT count(*) FROM WordQuiz wq WHERE wq.member = :member OR wq.member.role = 'ADMIN'")
     Integer countByMemberOrAdmin(Member member);
 
-    @EntityGraph(attributePaths = {"words"})
-    @Query("SELECT wq FROM WordQuiz wq WHERE wq.quizId = :quizId AND wq.member = :member OR wq.member.role = 'ADMIN'"  )
+    @Query("SELECT wq FROM WordQuiz wq JOIN FETCH wq.words WHERE wq.quizId = :quizId AND wq.member = :member OR wq.member.role = 'ADMIN'")
     List<WordQuiz> findSingleResultByMember(Member member, Long quizId);
 
     Optional<WordQuiz> findByQuizIdAndMember_MemberId(Long quizId, Long memberId);
